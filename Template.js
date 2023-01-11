23da72ce1216783fbf98f2e06f94ea37a884466f
Nixgit2000 <fourwaycoordination@gmail.com>
2023-01-11 14:07:18 -0500

    commit

--- Template.js
+++ Template.js
@@ -1,5 +1,5 @@
 function function1() {
-    var RegexExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    var RegexExp = /hello/g
     var input1 = document.getElementById("input1").value;
     if (RegexExp.test(input1)) {
         document.getElementById("output").innerHTML = "True";
